bc02db545da84ae3c51f28056ea74759c4170b65
Maria Gomez Roman <marigome@c3r1s3.42malaga.com>
2024-05-27 12:46:40 +0200

    get_next_line

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -10,93 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "get_next_line.h"
-#include <unistd.h> //write
-#include <stdlib.h> //malloc
-#include <stdio.h> //printf
-#include <fcntl.h> //Para leer archivos
+#include "get_next_line_bonus.h"
 
 
-
-size_t	ft_strlen(char *src)
-{
-	size_t	i;
-
-	i = 0;
-	while (src[i])
-		i++;
-	return (i);
-}
-
-void	ft_bzero(void *ptr, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		((char *)ptr)[i] = '\0';
-		i++;
-	}
-}
-
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*ptr;
-	size_t	i;
-
-	i = 0;
-	ptr = (void *)malloc(size * nmemb);
-	if (!ptr)
-		return (NULL);
-	ft_bzero(ptr, size * nmemb);
-	return (ptr);
-}
-char	*ft_strchr(char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == (char)c)
-			return (&s[i]);
-		i++;
-	}
-	if (s[i] == (char)c)
-		return (&s[i]);
-	return (0);
-}
-
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	size_t	i;
-	size_t	j;
-	size_t	total_len;
-	char	*buffer;
-
-	i = 0;
-	j = 0;
-	total_len = ft_strlen(s1) + ft_strlen(s2);
-	buffer = (char *)malloc(sizeof(char) * (total_len + 1));
-	if (!buffer || !s1 || !s2)
-		return (NULL);
-	while (s1[i])
-	{
-		buffer[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-	{
-		buffer[i + j] = s2[j];
-		j++;
-	}
-	buffer[i + j] = '\0';
-	return (buffer);
-}
-
-char	*ft_free_strjoin(char *buffer, char *tmp)
+static char	*ft_free_strjoin(char *buffer, char *tmp)
 {
 	char	*res;
 	
@@ -134,7 +51,7 @@ static char	*ft_new_buffer(char *buffer)
 	return (new_buffer);
 }
 
-char	*ft_get_line(char *buffer)
+static char	*ft_get_line(char *buffer)
 {
 	char	*line;
 	int	i;
@@ -186,44 +103,18 @@ static char	*ft_read_from_buffer(char *buffer, int fd)
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	*buffer[FT_SETSIZE];
 	char		*line;
 
-	if (!buffer)
-		buffer = ft_calloc(sizeof(char), (1));
+	if (!buffer[fd])
+		buffer[fd] = ft_calloc(sizeof(char), (1));
 	if (fd < 0 || 42 < 0 || read(fd, 0, 0) < 0)
 		return (0);
-	buffer = ft_read_from_buffer(buffer, fd);
-	if (!buffer)
+	buffer[fd] = ft_read_from_buffer(buffer, fd);
+	if (!buffer[fd])
 		return (NULL);
-	line = ft_get_line(buffer);
-	buffer = ft_new_buffer(buffer);
+	line = ft_get_line(buffer[fd]);
+	buffer[fd] = ft_new_buffer(buffer[fd]);
 	return (line);
 }
 
-int main (void)
-{
-	int fd;
-	char	*line;
-	int	count;
-
-	count = 0;
-
-	fd = open("new.txt", O_RDONLY);
-	printf("fd:%d\n", fd);
-	if (fd == -1)
-	{	
-		printf("Error al abrir el archivo");
-		return (0);
-	}
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (line == NULL)
-			break ;
-		printf("%s", line);
-		free(line);
-	}
-	close (fd);
-	return (0);
-}
