8b5e27232791579fea3e262f41e7fbe56fb039fa
marigome <marigome@student.42malaga.com>
2025-01-14 17:48:04 +0100

    ok

--- cub3d/includes/cub3D.h
+++ cub3d/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: marigome <marigome@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:38:05 by marigome          #+#    #+#             */
-/*   Updated: 2025/01/14 17:07:48 by marigome         ###   ########.fr       */
+/*   Updated: 2025/01/14 17:46:23 by marigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@
 # include <stdlib.h> // To use malloc, free
 # include <string.h> // To use strlen, strdup
 # include <unistd.h>
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "get_next_line_bonus.h"
+# include "colors.h"
+# include "macro.h"
 
 #endif
